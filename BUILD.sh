6c5fa854304b7522d44ab13821dddb8a7d9b2be8
root <root@lxc-docs.phrk.org>
2026-04-07 23:39:39 +1000

    chore: cleanup

--- BUILD.sh
+++ BUILD.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
+systemctl stop scrapegoat.service
+sync
+sleep 1
+git pull
+sync
+sleep 1
 docker system prune --all -f
 docker compose -f ./docker-compose-build-containers.yml build --no-cache
+sync
+sleep 1
+systemctl start scrapegoat.service
